bf130f39df5dfb1fd4d52a79b62b1ce00ca2d119
tr1ckyf0x <vladislav.lisyanskiy@gmail.com>
2018-01-26 13:24:45 +0700

    Initial Commit

---
+++ ZennexTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZennexTest
+//
+//  Created by Владислав Лисянский on 26/01/2018.
+//  Copyright © 2018 Владислав Лисянский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
