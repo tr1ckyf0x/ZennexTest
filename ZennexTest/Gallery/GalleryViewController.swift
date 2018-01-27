8159f644c708bf0b92d672f6841821080e7f5a61
tr1ckyf0x <vladislav.lisyanskiy@gmail.com>
2018-01-27 11:24:57 +0700

    Реализовано зуммирование

--- ZennexTest/Gallery/GalleryViewController.swift
+++ ZennexTest/Gallery/GalleryViewController.swift
@@ -14,7 +14,9 @@ import RxCocoa
 class GalleryViewController: UIViewController {
 
   @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var pageLabel: UILabel!
   var viewModel: GalleryViewModel?
+  var zoomDelegate = ZoomDelegate()
   var lastPage = 0
   
   override func viewDidLoad() {
@@ -26,17 +28,29 @@ class GalleryViewController: UIViewController {
       self.scrollView.subviews.forEach {
         $0.removeFromSuperview()
       }
+        
       self.scrollView.contentSize = CGSize(width: CGFloat(array.count) * self.scrollView.frame.width, height: self.scrollView.frame.height)
       self.scrollView.contentOffset = CGPoint(x: 0, y: 0)
+        
       
       for i in 0 ..< 2 {
-        let imageView = UIImageView()
-        imageView.tag = i
+        let imageScrollView = UIScrollView()
         let xPosition = self.scrollView.frame.width * CGFloat(i)
-        imageView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+        imageScrollView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+        imageScrollView.contentSize = CGSize(width: imageScrollView.frame.width, height: imageScrollView.frame.height)
+        imageScrollView.minimumZoomScale = 1
+        imageScrollView.maximumZoomScale = 5
+        imageScrollView.delegate = self.zoomDelegate
+        imageScrollView.showsVerticalScrollIndicator = false
+        imageScrollView.showsHorizontalScrollIndicator = false
+        imageScrollView.isPagingEnabled = false
+        imageScrollView.bounces = false
+        self.scrollView.addSubview(imageScrollView)
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 0, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
         imageView.backgroundColor = UIColor.black
         imageView.contentMode = .scaleAspectFit
-        self.scrollView.addSubview(imageView)
+        imageScrollView.addSubview(imageView)
         Alamofire.request(array[i])
           .responseData { response in
             guard let data = response.result.value
@@ -45,14 +59,19 @@ class GalleryViewController: UIViewController {
         }
       }
     })
-    
-    
+    .disposed(by: rx.disposeBag)
 
   }
 
   @IBAction func backButtonPressed(_ sender: Any) {
+    guard lastPage > 0 else { return }
+    scrollView.setContentOffset(CGPoint(x: CGFloat(lastPage - 1) * scrollView.frame.width, y: 0) , animated: true)
   }
   @IBAction func nextButtonPressed(_ sender: Any) {
+    guard let viewModel = viewModel,
+      lastPage < viewModel.imageUrls.value.count - 1
+      else { return }
+    scrollView.setContentOffset(CGPoint(x: CGFloat(lastPage + 1) * scrollView.frame.width, y: 0) , animated: true)
   }
   
 }
@@ -62,13 +81,51 @@ extension GalleryViewController: Identifiable {
 }
 
 extension GalleryViewController: UIScrollViewDelegate {
+  
+  func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+    addImagesNear()
+  }
+  
   func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    addImagesNear()
+  }
+  
+  func addImagesNear() {
     let currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
     guard currentPage != lastPage else { return }
     guard let viewModel = viewModel else { return }
     
     defer {
       lastPage = currentPage
+    }
+    
+    // Проверяем есть ли на текущей позиции картинка (если пролистнули быстро и загрузиться не успела)
+    let havePictureOnCurrentPage = scrollView.subviews
+      .contains { Int($0.frame.origin.x) == Int(CGFloat(currentPage) * scrollView.frame.width) }
+    if !havePictureOnCurrentPage {
+      let imageScrollView = UIScrollView()
+      let xPosition = scrollView.frame.width * CGFloat(currentPage)
+      imageScrollView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+      imageScrollView.contentSize = CGSize(width: imageScrollView.frame.width, height: imageScrollView.frame.height)
+      imageScrollView.minimumZoomScale = 1
+      imageScrollView.maximumZoomScale = 5
+      imageScrollView.delegate = self.zoomDelegate
+      imageScrollView.showsVerticalScrollIndicator = false
+      imageScrollView.showsHorizontalScrollIndicator = false
+      imageScrollView.isPagingEnabled = false
+      imageScrollView.bounces = false
+      scrollView.addSubview(imageScrollView)
+      let imageView = UIImageView()
+      imageView.frame = CGRect(x: 0, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+      imageView.backgroundColor = UIColor.black
+      imageView.contentMode = .scaleAspectFit
+      imageScrollView.addSubview(imageView)
+      Alamofire.request(viewModel.imageUrls.value[currentPage])
+        .responseData { response in
+          guard let data = response.result.value
+            else { print("Cannot load image"); return }
+          imageView.image = UIImage(data: data)
+      }
     }
     
     if currentPage > lastPage {
@@ -83,13 +140,23 @@ extension GalleryViewController: UIScrollViewDelegate {
       // Проверка что мы не с краю
       guard currentPage < viewModel.imageUrls.value.count - 1 else { return }
       // Добавить одну справа
-      let imageView = UIImageView()
-      imageView.tag = currentPage + 1
+      let imageScrollView = UIScrollView()
       let xPosition = scrollView.frame.width * CGFloat(currentPage + 1)
-      imageView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+      imageScrollView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+      imageScrollView.contentSize = CGSize(width: imageScrollView.frame.width, height: imageScrollView.frame.height)
+      imageScrollView.minimumZoomScale = 1
+      imageScrollView.maximumZoomScale = 5
+      imageScrollView.delegate = self.zoomDelegate
+      imageScrollView.showsVerticalScrollIndicator = false
+      imageScrollView.showsHorizontalScrollIndicator = false
+      imageScrollView.isPagingEnabled = false
+      imageScrollView.bounces = false
+      scrollView.addSubview(imageScrollView)
+      let imageView = UIImageView()
+      imageView.frame = CGRect(x: 0, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
       imageView.backgroundColor = UIColor.black
       imageView.contentMode = .scaleAspectFit
-      scrollView.addSubview(imageView)
+      imageScrollView.addSubview(imageView)
       Alamofire.request(viewModel.imageUrls.value[currentPage + 1])
         .responseData { response in
           guard let data = response.result.value
@@ -99,7 +166,7 @@ extension GalleryViewController: UIScrollViewDelegate {
     } else {
       // Удалить все слева
       scrollView.subviews
-      .filter { $0.frame.origin.x < scrollView.frame.size.width * CGFloat(currentPage) }
+        .filter { $0.frame.origin.x < scrollView.frame.size.width * CGFloat(currentPage) }
         .forEach { $0.removeFromSuperview() }
       // Удалить все что правее current + 1
       scrollView.subviews
@@ -108,13 +175,23 @@ extension GalleryViewController: UIScrollViewDelegate {
       // Проверка что мы не с краю
       guard currentPage > 0 else { return }
       // Добавить одну слева
-      let imageView = UIImageView()
-      imageView.tag = currentPage - 1
+      let imageScrollView = UIScrollView()
       let xPosition = scrollView.frame.width * CGFloat(currentPage - 1)
-      imageView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+      imageScrollView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+      imageScrollView.contentSize = CGSize(width: imageScrollView.frame.width, height: imageScrollView.frame.height)
+      imageScrollView.minimumZoomScale = 1
+      imageScrollView.maximumZoomScale = 5
+      imageScrollView.delegate = self.zoomDelegate
+      imageScrollView.showsVerticalScrollIndicator = false
+      imageScrollView.showsHorizontalScrollIndicator = false
+      imageScrollView.isPagingEnabled = false
+      imageScrollView.bounces = false
+      scrollView.addSubview(imageScrollView)
+      let imageView = UIImageView()
+      imageView.frame = CGRect(x: 0, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
       imageView.backgroundColor = UIColor.black
       imageView.contentMode = .scaleAspectFit
-      scrollView.addSubview(imageView)
+      imageScrollView.addSubview(imageView)
       Alamofire.request(viewModel.imageUrls.value[currentPage - 1])
         .responseData { response in
           guard let data = response.result.value
@@ -123,4 +200,25 @@ extension GalleryViewController: UIScrollViewDelegate {
       }
     }
   }
+  
+}
+
+class ZoomDelegate: NSObject, UIScrollViewDelegate {
+  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+    return scrollView.subviews.first
+  }
+
+  func scrollViewDidZoom(_ scrollView: UIScrollView) {
+    // Центрирование изображения по центру при уменьшении
+    var top = CGFloat(0)
+    var left = CGFloat(0)
+    if scrollView.contentSize.width < scrollView.bounds.size.width {
+      left = (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5
+    }
+    if scrollView.contentSize.height < scrollView.bounds.size.height {
+      top = (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5
+    }
+    scrollView.contentInset = UIEdgeInsetsMake(top, left, top, left)
+  }
+  
 }
